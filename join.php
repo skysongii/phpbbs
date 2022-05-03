fad43a26eed7bcabbaefd27e9d4908cf122f8e98
DESKTOP-3POO2SB\sunho <tjsgh9414@naver.com>
2022-05-03 13:56:56 +0900

    회원가입

--- join.php
+++ join.php
@@ -9,9 +9,10 @@
         <div align="center">
                 <p>회원가입</p>
                 <form method='post' action='join_ok.php'>
-                        <p>ID: <input type="text" name="id" autocomplete="off"></p>
-                        <p>PW: <input type="password" name="pw"></p>
-                        <p>Email: <input type="email" name="email"></p>
+                        <p>NAME: <input type="text" name="mb_name" autocomplete="off"></p>
+                        <p>ID: <input type="text" name="mb_id" autocomplete="off"></p>
+                        <p>PW: <input type="password" name="mb_pw"></p>
+                        <p>Email: <input type="email" name="mb_email"></p>
                         <input type="submit" value="회원가입">
                 </form>
         </div>
